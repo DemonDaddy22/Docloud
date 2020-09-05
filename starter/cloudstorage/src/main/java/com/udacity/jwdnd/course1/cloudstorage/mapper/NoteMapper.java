9d8a22539fae4ac85526a2b11910cf36b75c85da
DemonDaddy22 <guptarohan369@gmail.com>
2020-09-05 22:57:55 +0530

    add note service

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -1,10 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -16,4 +13,13 @@ public interface NoteMapper {
     @Insert("INSERT INTO NOTES(notetitle, notedescription, userId) VALUES (#{notetitle}, #{notedescription}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
     public int addNote(Note note);
+
+    @Select("SELECT * FROM NOTES WHERE noteId = #{noteId}")
+    public Note getNodeById(Integer noteId);
+
+    @Delete("DELETE FROM NOTES WHERE noteId = #{noteId}")
+    public int deleteNote(Integer noteId);
+
+    @Update("UPDATE NOTES SET notetitle = #{notetitle}, notedescription = #{notedescription} WHERE noteId = #{noteId}")
+    public int updateNote(Integer noteId);
 }
