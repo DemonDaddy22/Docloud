0d368a0b121282fc36b50fdcd9b6507b61befee1
DemonDaddy22 <guptarohan369@gmail.com>
2020-09-07 21:03:10 +0530

    add get mapping for home controller

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,15 +1,50 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+    private UserService userService;
+    private FileService fileService;
+    private NoteService noteService;
+    private CredentialService credentialService;
+
+    public HomeController(UserService userService, FileService fileService, NoteService noteService, CredentialService credentialService) {
+        this.userService = userService;
+        this.fileService = fileService;
+        this.noteService = noteService;
+        this.credentialService = credentialService;
+    }
 
     @GetMapping
-    public String homePage() {
+    public String homePage(Authentication authentication, Model model) {
+        String username = authentication.getName();
+        User user = this.userService.getUser(username);
+        Integer userId = user.getUserId();
+
+        List<File> files = this.fileService.getFiles(userId);
+        List<Note> notes = this.noteService.getNotes(userId);
+        List<Credential> credentials = this.credentialService.getCredentials(userId);
+
+        model.addAttribute("files", files);
+        model.addAttribute("notes", notes);
+        model.addAttribute("credentials", credentials);
+
         return "home";
     }
 }
